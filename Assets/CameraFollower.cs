0e11f224697c9c212d4260a7b324cc2a0d74e53d
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-05-17 21:40:12 +0200

    Camera follower

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    private GameObject _target;
+    
+    public void SetTarget(GameObject target)
+    {
+        _target = target;
+    }
+
+    private void Update()
+    {
+        if (_target != null)
+        {
+            transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, -10);
+        }
+    }
+}
--- Assets/Content/Scripts/Controllers/ShipController.cs
+++ Assets/Content/Scripts/Controllers/ShipController.cs
@@ -52,29 +52,52 @@ public class ShipController : MonoBehaviour, IController
     public TurningRate turningRate;
 
     private GameObject _ship;
+    private Rigidbody2D _rb2d;
     private GameObject _target;
     private Camera _camera;
+
+    private float movementFactor = 3;
+    private float movementThreshold = 10;
+    private float accel = 10;
     #endregion
     private void Start()
     {
         SetThrustVectors();
         SetTurningRateVectors();
         _ship = transform.parent.gameObject;
+        _rb2d = _ship.GetComponent<Rigidbody2D>();
         _camera = Camera.main;
         _target = GameObject.Find("Target");
+        _camera.gameObject.GetComponent<CameraFollower>().SetTarget(_ship);
     }
 
     void Update()
     {
+        switch (aimingMode)
+        {
+            case AimingMode.Cursor:
+                _ship.transform.rotation = RotationUtilities.MouseLookAtRotation(_ship, _camera, turningRate.leftTurningRate, turningRate.rightTurningRate);
+                break;
+            case AimingMode.Keyboard:
+                break;
+        }
         //rotates the ship towards the cursor
-        //_ship.transform.rotation = RotationUtilities.MouseLookAtRotation(_ship, _camera, turningRate.leftTurningRate, turningRate.rightTurningRate);
-        _ship.transform.rotation = RotationUtilities.ObjectLookAtRotation(_ship, _target, 10f);
+        
+
+
+        //_ship.transform.rotation = RotationUtilities.ObjectLookAtRotation(_ship, _target, 10f);
     }
 
+    void FixedUpdate()
+    {
+        Vector2 input = Input.
+
+        _ship.transform.Translate(movementFactor * thrust.forwardThrust, 0.0f, 0.0f);
+    }
     public void SetThrustVectors(float[] thrustVectors = null)
     {
         //if no argument has been given, thrust vectors will be set to default values
-        thrustVectors = thrustVectors ?? new float[4] { 3f, 1f, 1f, 1f };
+        thrustVectors = thrustVectors ?? new float[4] { 0.1f, 1f, 1f, 1f };
         thrust.forwardThrust = thrustVectors[0];
         thrust.backwardsThrust = thrustVectors[1];
         thrust.leftThrust = thrustVectors[2];
