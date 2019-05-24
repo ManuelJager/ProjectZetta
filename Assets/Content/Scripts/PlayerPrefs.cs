b6269a41463d10da587ad0f7b49ad1d7f9db1432
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-05-24 23:17:18 +0200

    Sabot round casing spread

--- Assets/Content/Scripts/PlayerPrefs.cs
+++ Assets/Content/Scripts/PlayerPrefs.cs
@@ -5,6 +5,7 @@ public class PlayerPrefs : MonoBehaviour
     public bool debug1;
     public bool debug2;
     public bool debug3;
+    public bool sabotRoundSpread;
     public static PlayerPrefs Instance;
     private void Awake()
     {
--- Assets/Content/Scripts/Projectiles/CannonProjectile.cs
+++ Assets/Content/Scripts/Projectiles/CannonProjectile.cs
@@ -3,6 +3,8 @@ public class CannonProjectile : MonoBehaviour, IProjectile
 {
     private int sourceGridID;
     private float projectileDamage;
+    [SerializeField]
+    private GameObject frontCasing, backCasing;
     public void ApplyDamage(int colliderGridID, IBlock block)
     {
         
@@ -41,6 +43,17 @@ public class CannonProjectile : MonoBehaviour, IProjectile
         this.sourceGridID = sourceGridID;
         this.projectileDamage = projectileDamage;
         GetComponent<Rigidbody2D>().AddForce(transform.right * force);
+        if (PlayerPrefs.Instance.sabotRoundSpread)
+        {
+            var backCasing = Instantiate(this.backCasing);
+            backCasing.transform.position = transform.position;
+            backCasing.transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 30f);
+            backCasing.GetComponent<Rigidbody2D>().AddForce(backCasing.transform.right * force * 0.6f);
+            var frontCasing = Instantiate(this.frontCasing);
+            frontCasing.transform.position = transform.position;
+            frontCasing.transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z - 30f);
+            frontCasing.GetComponent<Rigidbody2D>().AddForce(frontCasing.transform.right * force * 0.6f);
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
