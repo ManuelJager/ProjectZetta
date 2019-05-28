3fd321be975336620df4a72a54f3849101ba9f02
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2019-05-29 00:11:36 +0200

    28/05

---
+++ Assets/Content/Scripts/Interfaces/IProjectileWeapon.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IProjectileWeapon
+{
+    float projectileSpeed { get; }
+    float projectilePenetration { get; }
+}
